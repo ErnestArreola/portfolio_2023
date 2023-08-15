08d645c29680ed38a6581714fff937735a77f102
ErnestArreola <arreern@gmail.com>
2023-08-14 21:28:20 -0700

    Fixed hidden work on mobile

--- src/constants/index.js
+++ src/constants/index.js
@@ -103,20 +103,8 @@ const technologies = [
     icon: mongodb,
   },
   {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
     name: "git",
     icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
   },
 ];
 
